ce406cacc3918bb58f2e1442b901beb86d505c9b
Ruben <rvazquezcaceres@gmail.com>
2019-10-28 22:38:13 +0100

    Migration Passwords

---
+++ database/migrations/2019_10_28_213442_create_passwords_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('password');
+            $table->unsignedInteger('id_category');
+            $table->foreign('id_category')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passwords');
+    }
+}
